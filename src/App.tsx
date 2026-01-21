f7cd8e37ea78c34680512c0d5c9651fe62b12c46
blyxnovastudio <santiagosanchez.mdc@gmail.com>
2026-01-21 00:51:08 -0300

    Fix: Configuración para repositorio de usuario blyxnovastudio.github.io

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <CartDrawer />
-      <BrowserRouter basename="/las-marianas">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
